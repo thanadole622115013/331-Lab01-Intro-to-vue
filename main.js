580ec367fd3fd7a4e562687f4b915f4d181b827e
Thanadole Bunyamark <spikeman147@gmail.com>
2023-06-27 18:51:57 +0700

    3.6

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const { createApp, ref } = Vue
 createApp({
     setup(){
         const product = ref('Boots')
+        const image = ref('./assets/images/socks_green.jpg')
         const desc = ref('It is the product...')
+        const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
         return {
             product,
-            desc
+            image,
+            desc,
+            link
         }
     }
 }).mount('#app')
