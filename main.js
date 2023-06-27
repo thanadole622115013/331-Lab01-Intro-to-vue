d2828ac9cfaffede7df2c4ba598d79d6cc90e840
Thanadole Bunyamark <spikeman147@gmail.com>
2023-06-27 19:58:34 +0700

    4.9

--- main.js
+++ main.js
@@ -5,11 +5,17 @@ createApp({
         const image = ref('./assets/images/socks_green.jpg')
         const desc = ref('It is the product...')
         const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
+        const inStock = ref(false)
+        const inventory = ref(100)
+        const onSale = ref(true)
         return {
             product,
             image,
             desc,
-            link
+            link,
+            inStock,
+            inventory,
+            onSale
         }
     }
 }).mount('#app')
