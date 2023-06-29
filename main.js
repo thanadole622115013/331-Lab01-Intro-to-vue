0e4c7f2e436bd9d1cdaf8fefda645e70201c270c
Thanadole Bunyamark <spikeman147@gmail.com>
2023-06-29 16:29:29 +0700

    9.9

--- main.js
+++ main.js
@@ -1,75 +1,14 @@
-const { createApp, ref, computed } = Vue
-createApp({
+const { createApp, ref, computed} = Vue;
+const app = createApp({
     setup(){
-        const product = ref('Boots')
-        const brand = ref('SE 331')
-        // const image = ref('./assets/images/socks_green.jpg')
-        const desc = ref('It is the product...')
-        const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
-        // const inStock = ref(false)
-        const inventory = ref(100)
-        const onSale = ref(true)
-        const details = ref([
-            '50% cotton',
-            '30% wool',
-            '20% polyester'
-        ])
-        const variants = ref([
-            { id: 2234, color: 'green', image: '/assets/images/socks_green.jpg', quantity: 50},
-            { id: 2235, color: 'blue', image: '/assets/images/socks_blue.jpg', quantity: 0}
-        ])
-        const selectedVariant = ref(0)
-        const sizes = ref([
-            'S',
-            'M',
-            'L'
-        ])
         const cart = ref(0)
-        
-            function addToCart(){
-            cart.value +=1
-            }
-            
-            const saleState = computed(() => {
-                return brand.value + ' ' + product.value + ' ' + 'is on sale'
-            })
-
-            const title = computed(() => {
-                return brand.value + ' ' + product.value
-            })
-
-            function updateVariant(index){
-                selectedVariant.value = index;
-            }
-            const image = computed(() => {
-                return variants.value[selectedVariant.value].image
-            })
-            const inStock = computed(() => {
-                return variants.value[selectedVariant.value].quantity
-            })
-
-            function updateImage(variantImage){
-                image.value = variantImage
-            }
-            function updateInStock(){
-                this.inStock = !this.inStock
-            }
+        const premium = ref(false)
         return {
-            title,
-            saleState,
-            image,
-            desc,
-            link,
-            inStock,
-            inventory,
-            onSale,
-            details,
-            variants,
-            sizes,
             cart,
-            addToCart,
-            updateImage,
-            updateInStock
+            premium
         }
     }
-}).mount('#app')
+})
+
+app.component('product-display', productDisplay)
+app.mount('#app')
